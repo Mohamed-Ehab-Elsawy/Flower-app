2eba0bfbd65ab90f95fdfb445b7a354941d78acd
Abdelrahmn ayman <boodyayman936@gmail.com>
2025-12-30 01:54:33 +0200

    feat: enhance BestSellerEntity with additional fields for comprehensive product details

--- lib/features/home/domain/entities/best_seller_entity.dart
+++ lib/features/home/domain/entities/best_seller_entity.dart
@@ -1,9 +1,44 @@
 import 'package:equatable/equatable.dart';
 
 class BestSellerEntity extends Equatable {
-  String? massage;
+  final String id;
+  final String title;
+  final String description;
+  final String imgCover;
+  final List<String> images;
+  final num price;
+  final num priceAfterDiscount;
+  final double discountPercentage;
+  final int quantity;
+  final int sold;
+  final num rateAvg;
+
+  const BestSellerEntity({
+    required this.id,
+    required this.title,
+    required this.description,
+    required this.imgCover,
+    required this.images,
+    required this.price,
+    required this.priceAfterDiscount,
+    required this.discountPercentage,
+    required this.quantity,
+    required this.sold,
+    required this.rateAvg,
+  });
 
   @override
-  // TODO: implement props
-  List<Object?> get props => throw UnimplementedError();
+  List<Object?> get props => [
+    id,
+    title,
+    description,
+    imgCover,
+    images,
+    price,
+    priceAfterDiscount,
+    discountPercentage,
+    quantity,
+    sold,
+    rateAvg,
+  ];
 }
