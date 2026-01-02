0e31092bbf33f44ccf15317ca340b845409ab119
Mohamed Kamal <mahamedkamall47@gmail.com>
2026-01-02 23:38:40 +0200

    - Fix and Refactor Product Details Screen

--- lib/features/product_details/presentation/views/product_details_view.dart
+++ lib/features/product_details/presentation/views/product_details_view.dart
@@ -5,8 +5,12 @@ import 'package:flower_app/core/widgets/custom_image_view.dart';
 import 'package:flutter/material.dart';
 
 class ProductDetailsView extends StatefulWidget {
-  final ProductsEntity ?product;
-  const ProductDetailsView({super.key, this.product});
+  final ProductsEntity product;
+
+  const ProductDetailsView({
+    super.key,
+    required this.product,
+  });
 
   @override
   State<ProductDetailsView> createState() => _ProductDetailsViewState();
@@ -16,106 +20,134 @@ class _ProductDetailsViewState extends State<ProductDetailsView> {
   final PageController _pageController = PageController();
   int _currentPage = 0;
 
-  late final List<String> _images = widget.product?.images??[];
-  late final isInStock = (widget.product?.quantity ?? 0) > 0;
-
-
-  @override
-  void dispose() {
-    _pageController.dispose();
-    super.dispose();
-  }
-
   @override
   Widget build(BuildContext context) {
+    final images = widget.product.images;
+    final isInStock = widget.product.quantity! > 0;
     return Scaffold(
-      backgroundColor: context.appTheme.lightPink,
       body: SafeArea(
-        child: Column(
-          children: [
-            SizedBox(
-              height: 340,
-              child: Stack(
-                children: [
-                  IconButton(
-                    icon: const Icon(Icons.arrow_back_ios_new,
-                      color: Colors.black,
-                      size: 20,
-                    ),
-                    onPressed: () => Navigator.pop(context),
-                  ),
-                  PageView.builder(
-                    controller: _pageController,
-                    onPageChanged: (index) => setState(() => _currentPage = index),
-                    itemCount: _images.length,
-                    itemBuilder: (context, index) {
-                      return Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 6),
-                        child: CustomImageView(
-                          imagePath: _images[index],
+        child: CustomScrollView(
+          slivers: [
+            SliverAppBar(
+              pinned: true,
+              automaticallyImplyLeading: false,
+              expandedHeight: 360,
+              surfaceTintColor: Colors.transparent,
+              backgroundColor: Colors.white,
+              scrolledUnderElevation: 0,
+              flexibleSpace: FlexibleSpaceBar(
+                background: Stack(
+                  fit: StackFit.expand,
+                  children: [
+                    PageView.builder(
+                      controller: _pageController,
+                      onPageChanged: (index) => setState(() => _currentPage = index),
+                      itemCount: images!.length,
+                      itemBuilder: (context, index) {
+                        return CustomImageView(
+                          imagePath: images[index],
                           fit: BoxFit.cover,
+                        );
+                      },
+                    ),
+                    Positioned(
+                      top: 8,
+                      left: 8,
+                      child: IconButton(
+                        icon: const Icon(
+                          Icons.arrow_back_ios_new,
+                          color: Colors.black,
+                          size: 20,
                         ),
-                      );
-                    },
-                  ),
-                ],
+                        onPressed: () => Navigator.pop(context),
+                      ),
+                    ),
+
+                    Positioned(
+                      bottom: 12,
+                      left: 0,
+                      right: 0,
+                      child: _buildIndicatorWidget(images.length),
+                    ),
+                  ],
+                ),
               ),
             ),
-            _buildPageIndicators(),
-            const SizedBox(height: 12),
-            Expanded(
-              child: Container(
-                color: Colors.white,
-                child: Padding(
-                  padding: const EdgeInsets.all(16),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text("${"EGP".tr()} ${widget.product?.price}",style: context.appTheme.medium20.copyWith(
+
+            SliverToBoxAdapter(
+              child: Padding(
+                padding: const EdgeInsets.all(20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      mainAxisAlignment:
+                      MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          "${"EGP".tr()} ${widget.product.price}",
+                          style: context.appTheme.medium20.copyWith(
                             fontWeight: FontWeight.bold,
-                          )),
-                          RichText(text:
-                          TextSpan(
+                          ),
+                        ),
+                        RichText(
+                          text: TextSpan(
                             children: [
                               TextSpan(
                                 text: "status".tr(),
-                                style: context.appTheme.medium16,
+                                style:
+                                context.appTheme.medium16,
                               ),
                               TextSpan(
-                                text: isInStock ? "inStock".tr() : "outOfStock".tr(),
-                                style: context.appTheme.regular14.copyWith(
-                                  color: isInStock ? context.appTheme.success : context.appTheme.error,
+                                text: isInStock
+                                    ? "inStock".tr()
+                                    : "outOfStock".tr(),
+                                style: context.appTheme
+                                    .regular14
+                                    .copyWith(
+                                  color: isInStock
+                                      ? context.appTheme.success
+                                      : context.appTheme.error,
                                 ),
                               ),
-                            ]
-                          )),
-                        ],
-                      ),
-                      const SizedBox(height: 4),
-                      Text("allPricesIncludeTax".tr(),style:context.appTheme.regular12.copyWith(
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 8),
+                    Text(
+                      "allPricesIncludeTax".tr(),
+                      style: context.appTheme.regular12.copyWith(
                         color: context.appTheme.grey,
-                      ),),
-                      const SizedBox(height: 8),
-                      Text(widget.product?.title ?? "",style: context.appTheme.medium16.copyWith(
-                        color: context.appTheme.surface
-                      )),
-                      const SizedBox(height: 24),
-                      Text("description".tr(),style: context.appTheme.medium16),
-                      const SizedBox(height: 8),
-                      Text(widget.product?.description ??"",style: context.appTheme.regular14),
-                      const SizedBox(height: 24),
-                      Text("bouquetInclude".tr(),style: context.appTheme.medium16),
-                      const SizedBox(height: 8),
-                      Text("${"quantity".tr()}: ${widget.product?.quantity ?? 0}",style: context.appTheme.regular14),
-                      const SizedBox(height: 2),
-                      Text("whiteWrap".tr(),style: context.appTheme.regular14),
-                      const SizedBox(height: 24),
-                      ElevatedButton(onPressed: (){}, child: Text("addToCart".tr()))
-                    ],
-                  ),
+                      ),
+                    ),
+                    const SizedBox(height: 16),
+                    Text(
+                      widget.product.title??"",
+                      style: context.appTheme.medium16,
+                    ),
+                    const SizedBox(height: 24),
+                    Text("description".tr(),
+                        style: context.appTheme.medium16),
+                    const SizedBox(height: 8),
+                    Text(widget.product.description??"",
+                        style: context.appTheme.regular14),
+
+                    const SizedBox(height: 24),
+                    Text("bouquetInclude".tr(),
+                        style: context.appTheme.medium16),
+                    const SizedBox(height: 8),
+                    Text(
+                      "${"quantity".tr()}: ${widget.product.quantity}",
+                      style: context.appTheme.regular14,
+                    ),
+                    const SizedBox(height: 32),
+                    ElevatedButton(
+                      onPressed: isInStock ? () {} : null,
+                      child: Text("addToCart".tr()),
+                    ),
+                  ],
                 ),
               ),
             ),
@@ -125,33 +157,24 @@ class _ProductDetailsViewState extends State<ProductDetailsView> {
     );
   }
 
-  Widget _buildPageIndicators() {
-    return Padding(
-      padding: const EdgeInsets.only(top: 16),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: List.generate(
-          _images.length,
-              (index) => GestureDetector(
-            onTap: () {
-              _pageController.animateToPage(
-                index,
-                duration: const Duration(milliseconds: 300),
-                curve: Curves.easeInOut,
-              );
-            },
-            child: AnimatedContainer(
-              duration: const Duration(milliseconds: 300),
-              margin: const EdgeInsets.symmetric(horizontal: 4),
-              height: 8,
-              width: _currentPage == index ? 24 : 8,
-              decoration: BoxDecoration(
-                color: _currentPage == index
-                    ? context.appTheme.primary
-                    : context.appTheme.secondary[70],
-                borderRadius: BorderRadius.circular(4),
-              ),
-            ),
+  Widget _buildIndicatorWidget(int images) {
+    return  Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: List.generate(
+        images,
+            (index) => AnimatedContainer(
+          duration:
+          const Duration(milliseconds: 300),
+          margin:
+          const EdgeInsets.symmetric(horizontal: 4),
+          height: 8,
+          width:
+          _currentPage == index ? 24 : 8,
+          decoration: BoxDecoration(
+            color: _currentPage == index
+                ? context.appTheme.primary
+                : context.appTheme.secondary[70],
+            borderRadius: BorderRadius.circular(4),
           ),
         ),
       ),
