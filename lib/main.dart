046876ba985f51ce282e21626a6123347de40246
Abdelrahmn ayman <boodyayman936@gmail.com>
2026-01-04 02:54:48 +0200

    chore: remove unnecessary whitespace in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -28,14 +28,8 @@ void main() async {
       child: const FlowerApp(),
     ),
   );
-
-
-   
 }
-  
 
-
-  ؤؤؤ
 Future<bool> getInitialAppRoute() async {
   final rememberMe = await AppLocalStorage.getBool(LocalKeys.rememberMe);
   final token = await AppLocalStorage.getSecuredString(
