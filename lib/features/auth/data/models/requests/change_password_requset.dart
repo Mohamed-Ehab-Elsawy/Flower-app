8bdcbfbe714f0a88490c3c47ad8b4a9011bec98e
Abdelrahmn ayman <boodyayman936@gmail.com>
2026-01-12 22:08:09 +0200

    feat: finish change password logic and ui

---
+++ lib/features/auth/data/models/requests/change_password_requset.dart
@@ -0,0 +1,26 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'change_password_requset.g.dart';
+
+@JsonSerializable()
+class ChangePasswordRequest {
+  @JsonKey(name: "password")
+  final String? password;
+  @JsonKey(name: "newPassword")
+  final String? newPassword;
+
+  ChangePasswordRequest ({
+    this.password,
+    this.newPassword,
+  });
+
+  factory ChangePasswordRequest.fromJson(Map<String, dynamic> json) {
+    return _$ChangePasswordRequestFromJson(json);
+  }
+
+  Map<String, dynamic> toJson() {
+    return _$ChangePasswordRequestToJson(this);
+  }
+}
+
+
