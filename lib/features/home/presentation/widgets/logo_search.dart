19c01550f62c9918719e26b93724dfac915a5a87
Mohamed Kamal <mahamedkamall47@gmail.com>
2026-01-30 15:08:43 +0200

    feat: Navigate to search on search bar tap
    Implement navigation to the search screen when the user taps on the search text field on the home screen.

--- lib/features/home/presentation/widgets/logo_search.dart
+++ lib/features/home/presentation/widgets/logo_search.dart
@@ -2,13 +2,16 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:flower_app/core/app_extension/app_extension.dart';
 import 'package:flower_app/core/app_extension/app_spacing_extension.dart';
 import 'package:flower_app/core/constants/app_paths.dart';
+import 'package:flower_app/features/home/presentation/view_model/home_view_model.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class LogoAndSearch extends StatelessWidget {
   const LogoAndSearch({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final cubit = context.read<HomeViewModel>();
     return SliverPadding(
       padding: const EdgeInsets.symmetric(horizontal: 16),
       sliver: Column(
@@ -22,6 +25,7 @@ class LogoAndSearch extends StatelessWidget {
                 Image.asset(AppPaths.logo),
                 Expanded(
                   child: TextField(
+                    onTap: () => cubit.doEvent(NavigatorToSearch()),
                     onTapUpOutside: (event) {
                       FocusScope.of(context).unfocus();
                     },
