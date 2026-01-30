ec244ee61504da36371728f0785e97a72285bc6a
Abdo0Salah <abdelrahmansalah102@gmail.com>
2026-01-30 15:58:19 +0200

    refactor: update navigation logic in `payment_view.dart`
    - Replace `pushNamedAndRemoveUntil` with `pushReplacementNamed` when navigating after a successful payment.
    
    Signed-off-by: Abdo0Salah <abdelrahmansalah102@gmail.com>

--- lib/core/helper/app_routes.dart
+++ lib/core/helper/app_routes.dart
@@ -16,11 +16,11 @@ import 'package:flower_app/features/checkout/presentation/view/check_out_view.da
 import 'package:flower_app/features/checkout/presentation/view/payment_view.dart';
 import 'package:flower_app/features/home/presentation/view/best_seller_view.dart';
 import 'package:flower_app/features/home/presentation/view/occasions/occasion_screen.dart';
+import 'package:flower_app/features/home/presentation/view/search_view.dart';
 import 'package:flower_app/features/home/presentation/view_model/home_view_model.dart';
 import 'package:flower_app/features/orders/presentation/view_model/order_viewmodel.dart';
 import 'package:flower_app/features/product_details/presentation/views/product_details_view.dart';
 import 'package:flower_app/features/profile/presentation/cubit/about_us/about_us_view_model.dart';
-import 'package:flower_app/features/home/presentation/view/search_view.dart';
 import 'package:flower_app/features/profile/presentation/views/edit_profile/edit_profile_view.dart';
 import 'package:flower_app/features/profile/presentation/views/edit_profile/view_model/edit_profile_view_model.dart';
 import 'package:flower_app/features/profile/presentation/views/main_profile/about_us_view.dart';
@@ -158,8 +158,11 @@ Route? onGenerateRoute(RouteSettings settings) {
       );
     case AppRoutes.payment:
       return MaterialPageRoute(
-        builder: (_) => const PaymentView(),
         settings: settings,
+        builder: (_) => BlocProvider<OrderViewModel>.value(
+          value: getIt<OrderViewModel>(),
+          child: const PaymentView(),
+        ),
       );
     case AppRoutes.aboutUs:
       return MaterialPageRoute(
