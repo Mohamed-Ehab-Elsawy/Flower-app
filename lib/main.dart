1eb7f7a888876ed94a29f27595d37cda112b1de6
Mohamed Kamal <mahamedkamall47@gmail.com>
2026-02-03 00:32:26 +0200

    feat: Integrate Firebase Crashlytics
    Integrate Firebase Crashlytics for error reporting. This includes:
    - Adding the `firebase_crashlytics` dependency.
    - Initializing Crashlytics to automatically handle and record both synchronous (Flutter framework) and asynchronous errors.

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flower_app/core/bloc_box/my_bloc_observer.dart';
 import 'package:flower_app/core/di/di.dart';
 import 'package:flower_app/core/helper/app_local_storage.dart';
 import 'package:flower_app/core/helper/local_keys.dart';
 import 'package:flower_app/flower_app.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -14,6 +16,15 @@ bool isLoggedInUser = false;
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  // Sync Errors to Crashlytics - Main Thread errors
+  FlutterError.onError = (errorDetails) {
+    FirebaseCrashlytics.instance.recordFlutterFatalError(errorDetails);
+  };
+  // Async Errors to Crashlytics - OutSide Main Threads - PlatForm Specific Errors - Apis Requests Errors
+  PlatformDispatcher.instance.onError = (error, stack) {
+    FirebaseCrashlytics.instance.recordError(error, stack,fatal: true);
+    return true;
+  };
   await EasyLocalization.ensureInitialized();
   Bloc.observer = MyBlocObserver();
   isLoggedInUser = await getInitialAppRoute();
